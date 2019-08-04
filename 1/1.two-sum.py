0e464a2a5e0b330b68f096d9df3437d8cbcdff3c
Park GiSung <gdipig@naver.com>
2019-08-04 11:13:22 +0900

    Solved 1

---
+++ 1/1.two-sum.py
@@ -0,0 +1,42 @@
+#
+# @lc app=leetcode id=1 lang=python3
+#
+# [1] Two Sum
+#
+# https://leetcode.com/problems/two-sum/description/
+#
+# algorithms
+# Easy (42.49%)
+# Total Accepted:    1.6M
+# Total Submissions: 3.7M
+# Testcase Example:  '[2,7,11,15]\n9'
+#
+# Given an array of integers, return indices of the two numbers such that they
+# add up to a specific target.
+# 
+# You may assume that each input would have exactly one solution, and you may
+# not use the same element twice.
+# 
+# Example:
+# 
+# 
+# Given nums = [2, 7, 11, 15], target = 9,
+# 
+# Because nums[0] + nums[1] = 2 + 7 = 9,
+# return [0, 1].
+# 
+# 
+# 
+# 
+#
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        # return indexes
+        # just one solution
+        # 1. Brute-force
+        # search all elements in the list, and return two value
+        # Timeout, maybe
+        for i in range(len(nums)):
+            for j in range(i + 1, len(nums)):
+                if nums[i] + nums[j] == target:
+                    return [i, j]
